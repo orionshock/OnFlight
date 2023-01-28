2e6c4934e336c88e2e1585298e6a47602e7eddbd
OrionShock <orionshock@gmail.com>
2023-01-27 18:42:29 -0700

    Update InFlight_Load.lua
    remove unused hook

--- InFlight_Load/InFlight_Load.lua
+++ InFlight_Load/InFlight_Load.lua
@@ -126,34 +126,6 @@ if GetAddOnEnableState(UnitName("player"), "InFlight") == 2 then
 		}
 	}
 
-	-- Support flights that are started by gossip options properly so automation addons don't futz it.
-	-- do
-	-- 	local orig_SelectGossipOption = SelectGossipOption
-	-- 	function SelectGossipOption(option, ...)
-	-- 		--print("SelectGossipOption", option, ...)
-	-- 		local gossipOptions = C_GossipInfo.GetOptions()
-	-- 		local gossipText = gossipOptions[option].name
-	-- 		--print(gossipText, gossipType)
-	-- 		if (gossipText and gossipText ~= "") then
-	-- 			local gossipZoneData = gossipFlightData[GetMinimapZoneText()]
-	-- 			--print(gossipText, GetMinimapZoneText(), gossipZoneData)
-	-- 			if gossipZoneData then
-	-- 				for index, gossipFlightOption in ipairs(gossipZoneData) do
-	-- 					if strfind(gossipText, gossipFlightOption.find, 1, true) then
-	-- 						if gossipFlightOption.s and gossipFlightOption.d and LoadInFlight() then
-	-- 							if InFlight and InFlight.db.profile.chatlog then
-	-- 								print("|cff00ff40In|cff00aaffFlight|r: Special Flight - ", gossipFlightOption.s, "->", gossipFlightOption.d)
-	-- 							end
-	-- 							self:StartMiscFlight(gossipFlightOption.s, gossipFlightOption.d)
-	-- 						end
-	-- 					end
-	-- 				end
-	-- 			end
-	-- 		end
-	-- 		orig_SelectGossipOption(option, ...)
-	-- 	end
-	-- end
-
 	do
 		local orig_C_GossipInfo_SelectOption = C_GossipInfo.SelectOption
 		function C_GossipInfo.SelectOption(option, ...)
