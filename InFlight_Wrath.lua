59dc26e1528bee361db829c248443bc6c0249b41
OrionShock <orionshock@gmail.com>
2023-01-28 21:49:53 -0700

    split status bar out to own module
    many things can display bars and timers, core is going to handle hooks and back end.

--- InFlight_Wrath.lua
+++ InFlight_Wrath.lua
@@ -1,6 +1,13 @@
 --[[
     New InFlight System
-    We're Ace3'ing it, in for a peny, in for a pound.
+    Main Addon handles the Flight System
+        Hooks,
+        Figuring out when we're on flights and get off them
+        Send Events via AceEvent when things happen.
+
+        StatusBarMdoule handles the display
+            Quartz also can handle the display
+            --Maybe LDB will later?
 
 ]]
 --luacheck: globals LibStub InCombatLockdown
@@ -15,31 +22,9 @@ local LSM = LibStub("LibSharedMedia-3.0")
 local db
 local svDefaults = {
     profile = {
-        countUp = false,
-        fillUp = false,
-        showSpark = true,
-        backgroundColor = {r = 0, g = 0, b = 1, a = 1},
-        unknownFlightColor = {r = 1, g = 0, b = 0, a = 1},
-        barHeight = 12,
-        barWidth = 300,
-        barTexture = "Blizzard",
-        barColor = {r = 0, g = 1, b = 0, a = 1},
-        borderTexture = "Blizzard Dialog",
-        borderColor = {r = 0, g = 1, b = 0, a = 1},
-        compactMode = false,
-        fontName = "2002 Bold",
-        fontSize = 12,
-        fontColor = {r = 1.0, g = 1.0, b = 1.0, a = 1.0},
         showChat = true,
         confirmFlight = false,
-        barLocation = {
-            offsetX = 0,
-            offsetY = -170,
-            anchorPoint = "TOP",
-            relativePoint = "TOP"
-        }
     },
-    global = {}
 }
 addonCore.svDefaults = svDefaults
 
@@ -51,8 +36,6 @@ end
 function addonCore:OnEnable()
     LibStub("AceConfig-3.0"):RegisterOptionsTable(addonName, self.configOptionsTable)
     self:RegisterChatCommand("inflight", "ChatCommand")
-
-    self:SetupTimerBar()
 end
 
 function addonCore:OnDisable()
@@ -87,10 +70,8 @@ function addonCore:SetOption(info, ...)
         db.profile[info[#info]].b = blue
         db.profile[info[#info]].g = green
         db.profile[info[#info]].a = alpha
-        print("Option Color: " .. info[#info] .. " => " .. red, blue, green, alpha)
     else
         db.profile[info[#info]] = ...
-        print("Option " .. info[#info] .. " => " .. tostringall(...))
     end
 end
 
@@ -102,146 +83,6 @@ addonCore.configOptionsTable = {
     set = "SetOption",
     childGroups = "tab",
     args = {
-        barOptions = {
-            name = L["Bar Options"],
-            type = "group",
-            order = 100,
-            args = {
-                countUp = {
-                    hidden = true,
-                    name = L["Count Upwards"],
-                    type = "toggle",
-                    order = 10
-                },
-                fillUp = {
-                    hidden = true,
-                    name = L["Fill Upwards"],
-                    type = "toggle",
-                    order = 20
-                },
-                showSpark = {
-                    hidden = true,
-                    name = L["Show Spark"],
-                    type = "toggle",
-                    order = 30
-                },
-                backgroundColor = {
-                    hidden = true,
-                    name = L["Background Color"],
-                    type = "color",
-                    order = 40
-                },
-                unknownFlightColor = {
-                    name = L["Unknown Flight Color"],
-                    type = "color",
-                    order = 50
-                },
-                size = {
-                    hidden = true,
-                    name = L["Bar Size Settings"],
-                    type = "group",
-                    order = 60,
-                    inline = true,
-                    args = {
-                        barHeight = {
-                            name = L["Height"],
-                            type = "range",
-                            order = 10,
-                            min = 10,
-                            max = 300,
-                            step = 1
-                        },
-                        barWidth = {
-                            name = L["Width"],
-                            type = "range",
-                            order = 20,
-                            min = 10,
-                            max = 1000,
-                            step = 10
-                        }
-                    }
-                },
-                barSettings = {
-                    name = L["Bar Settings"],
-                    type = "group",
-                    order = 70,
-                    inline = true,
-                    width = "normal",
-                    args = {
-                        barTexture = {
-                            name = L["Bar Texture"],
-                            type = "select",
-                            order = 10,
-                            dialogControl = "LSM30_Statusbar",
-                            values = AceGUIWidgetLSMlists.statusbar
-                        },
-                        barColor = {
-                            name = L["Bar Color"],
-                            type = "color",
-                            order = 20
-                        }
-                    }
-                },
-                borderSettings = {
-                    hidden = true,
-                    name = L["Border Settings"],
-                    type = "group",
-                    order = 80,
-                    inline = true,
-                    width = "normal",
-                    args = {
-                        borderTexture = {
-                            name = L["Border Texture"],
-                            type = "select",
-                            order = 10,
-                            dialogControl = "LSM30_Border",
-                            values = AceGUIWidgetLSMlists.border
-                        },
-                        borderColor = {
-                            name = L["Border Color"],
-                            type = "color",
-                            order = 20
-                        }
-                    }
-                }
-            }
-        },
-        textOptions = {
-            name = "Text Options",
-            type = "group",
-            order = 200,
-            args = {
-                compactMode = {
-                    name = L["Compact Mode"],
-                    type = "toggle",
-                    order = 10
-                },
-                fontOptions = {
-                    name = L["Font Options"],
-                    type = "group",
-                    inline = true,
-                    args = {
-                        fontName = {
-                            name = L["Font"],
-                            type = "select",
-                            dialogControl = "LSM30_Font",
-                            values = AceGUIWidgetLSMlists.font,
-                            order = 10
-                        },
-                        fontSize = {
-                            name = L["Size"],
-                            type = "input",
-                            order = 20
-                        },
-                        fontColor = {
-                            name = L["Color"],
-                            type = "color",
-                            order = 30
-                        }
-                    }
-                }
-            }
-        },
         misc = {
             name = L["Misc Options"],
             type = "group",
@@ -259,112 +100,5 @@ addonCore.configOptionsTable = {
                 }
             }
         }
-    }
+    },
 }
-local function disp_time(time)
-    local minutes = floor(mod(time, 3600) / 60)
-    local seconds = floor(mod(time, 60))
-    return format("%d:%02d", minutes, seconds)
-end
-
-local function timerBarOnUpdate(self, elapsed)
-    if not self.timeRemaining then
-        self:Hide()
-        return
-    end
-    self.timeRemaining = self.timeRemaining - elapsed
-    if self.timeRemaining > 0 then
-        _G[self:GetName() .. "StatusBar"]:SetValue(self.timeRemaining)
-        _G[self:GetName() .. "Text"]:SetFormattedText("%s - %s", self.text, disp_time(self.timeRemaining))
-    else
-        self:Hide()
-        self.timeRemaining = 0
-        self.duration = 0
-        self.text = ""
-        _G[self:GetName() .. "Text"]:SetText("")
-    end
-end
-
-function addonCore:SetupTimerBar()
-    local barLocation = db.profile.barLocation
-    local InFlightTimerFrame = CreateFrame("Frame", "InFlightTimerFrame", UIParent, "MirrorTimerTemplate")
-    InFlightTimerFrame:UnregisterAllEvents()
-    InFlightTimerFrame:SetScript("OnEvent", nil)
-    InFlightTimerFrame:SetScript("OnUpdate", timerBarOnUpdate)
-    InFlightTimerFrame:Hide()
-    InFlightTimerFrame:SetPoint(barLocation.anchorPoint, UIParent, barLocation.relativePoint, barLocation.offsetX, barLocation.offsetY)
-    InFlightTimerFrame:SetMovable(true)
-    InFlightTimerFrame:EnableMouse(true)
-    InFlightTimerFrame:SetClampedToScreen(true)
-    InFlightTimerFrame:RegisterForDrag("LeftButton")
-
-    local statusBar = _G[InFlightTimerFrame:GetName() .. "StatusBar"]
-    statusBar:SetStatusBarColor(db.profile.barColor.r, db.profile.barColor.g, db.profile.barColor.b, db.profile.barColor.a)
-
-    InFlightTimerFrame:SetScript(
-        "OnMouseUp",
-        function(frame, button)
-            print(frame:GetName(), button)
-        end
-    )
-
-    InFlightTimerFrame:SetScript(
-        "OnDragStart",
-        function(frame)
-            if IsShiftKeyDown() then
-                frame:StartMoving()
-            end
-        end
-    )
-    InFlightTimerFrame:SetScript(
-        "OnDragStop",
-        function(frame)
-            frame:StopMovingOrSizing()
-            local a, b, c, d, e = frame:GetPoint()
-            db.p, db.rp, db.x, db.y = a, c, floor(d + 0.5), floor(e + 0.5)
-            local anchorPoint, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
-            db.profile.barLocation.anchorPoint = anchorPoint
-            db.profile.barLocation.relativePoint = relativePoint
-            db.profile.barLocation.offsetX = offsetX
-            db.profile.barLocation.offsetY = offsetY
-        end
-    )
-    self.InFlightTimerFrame = InFlightTimerFrame
-end
-
-function addonCore:StartTimerBar(text, duration, unknownFlightFlag) --Bar Text and Duration in seconds--
-    if not self.InFlightTimerFrame then
-        return
-    end
-    if (not text) or (not duration) then
-        return
-    end
-
-    local timerFrame = self.InFlightTimerFrame
-    timerFrame.duration = duration
-    timerFrame.timeRemaining = duration
-    timerFrame.text = text
-
-    local statusBar = _G[InFlightTimerFrame:GetName() .. "StatusBar"]
-    statusBar:SetMinMaxValues(0, duration)
-    statusBar:SetValue(duration)
-
-    if unknownFlightFlag then
-        statusBar:SetStatusBarColor(db.profile.unknownFlightColor.r, db.profile.unknownFlightColor.g, db.profile.unknownFlightColor.b, db.profile.unknownFlightColor.a)
-    else
-        statusBar:SetStatusBarColor(db.profile.barColor.r, db.profile.barColor.g, db.profile.barColor.b, db.profile.barColor.a)
-    end
-
-    local displayText = _G[timerFrame:GetName() .. "Text"]
-    displayText:SetFormattedText("%s - %s", text, disp_time(duration))
-
-    timerFrame:Show()
-end
-
-function addonCore:StopTimerBar()
-    self.InFlightTimerFrame:Hide()
-    self.InFlightTimerFrame.timeRemaining = 0
-    self.InFlightTimerFrame.duration = 0
-    self.InFlightTimerFrame.text = ""
-    _G[self.InFlightTimerFrame:GetName() .. "Text"]:SetText("")
-end
---
+++ Module_StatusBar.lua
@@ -0,0 +1,340 @@
+--[[
+    This Module's Entire Job is to handle the Status Bar
+]]
+local addonName, addonCore = ...
+local statusBarModuleCore = addonCore:NewModule("StatusBarModule", "AceEvent-3.0")
+
+local L = LibStub("AceLocale-3.0"):GetLocale("InFlight")
+local LSM = LibStub("LibSharedMedia-3.0")
+
+local db
+local svDefaults = {
+    profile = {
+        countUp = false,
+        fillUp = false,
+        showSpark = true,
+        backgroundColor = {r = 0, g = 0, b = 1, a = 1},
+        unknownFlightColor = {r = 1, g = 0, b = 0, a = 1},
+        barHeight = 12,
+        barWidth = 300,
+        barTexture = "Blizzard",
+        barColor = {r = 0, g = 1, b = 0, a = 1},
+        borderTexture = "Blizzard Dialog",
+        borderColor = {r = 0, g = 1, b = 0, a = 1},
+        compactMode = false,
+        fontName = "2002 Bold",
+        fontSize = 12,
+        fontColor = {r = 1.0, g = 1.0, b = 1.0, a = 1.0},
+        barLocation = {
+            offsetX = 0,
+            offsetY = -170,
+            anchorPoint = "TOP",
+            relativePoint = "TOP"
+        }
+    }
+}
+statusBarModuleCore.svDefaults = svDefaults
+
+function statusBarModuleCore:OnInitialize()
+    self.db = addonCore.db:RegisterNamespace("StatusBarModule", svDefaults)
+    db = self.db
+end
+function statusBarModuleCore:OnEnable()
+    self:SetupTimerBar()
+    self:RegisterMessage("InFlight_Start")
+    self:RegisterMessage("InFlight_Stop")
+end
+
+function statusBarModuleCore:InFlight_Start(event, text, duration, knownFlightFlag)
+    print(event, text, duration)
+    self:StartTimerBar(text, duration, knownFlightFlag)
+end
+
+function statusBarModuleCore:InFlight_Stop(event)
+    print(event)
+    self:StopTimerBar()
+end
+
+local function disp_time(time)
+    local minutes = floor(mod(time, 3600) / 60)
+    local seconds = floor(mod(time, 60))
+    return format("%d:%02d", minutes, seconds)
+end
+
+local function timerBarOnUpdate(self, elapsed)
+    if not self.timeRemaining then
+        self:Hide()
+        return
+    end
+    self.timeRemaining = self.timeRemaining - elapsed
+    if self.timeRemaining > 0 then
+        _G[self:GetName() .. "StatusBar"]:SetValue(self.timeRemaining)
+        _G[self:GetName() .. "Text"]:SetFormattedText("%s - %s", self.text, disp_time(self.timeRemaining))
+    else
+        self:Hide()
+        self.timeRemaining = 0
+        self.duration = 0
+        self.text = ""
+        _G[self:GetName() .. "Text"]:SetText("")
+    end
+end
+
+function statusBarModuleCore:SetupTimerBar()
+    local barLocation = db.profile.barLocation
+    local InFlightTimerFrame = CreateFrame("Frame", "InFlightTimerFrame", UIParent, "MirrorTimerTemplate")
+    InFlightTimerFrame:UnregisterAllEvents()
+    InFlightTimerFrame:SetScript("OnEvent", nil)
+    InFlightTimerFrame:SetScript("OnUpdate", timerBarOnUpdate)
+    InFlightTimerFrame:Hide()
+    InFlightTimerFrame:SetPoint(barLocation.anchorPoint, UIParent, barLocation.relativePoint, barLocation.offsetX, barLocation.offsetY)
+    InFlightTimerFrame:SetMovable(true)
+    InFlightTimerFrame:EnableMouse(true)
+    InFlightTimerFrame:SetClampedToScreen(true)
+    InFlightTimerFrame:RegisterForDrag("LeftButton")
+
+    local statusBar = _G[InFlightTimerFrame:GetName() .. "StatusBar"]
+    statusBar:SetStatusBarColor(db.profile.barColor.r, db.profile.barColor.g, db.profile.barColor.b, db.profile.barColor.a)
+
+    InFlightTimerFrame:SetScript(
+        "OnMouseUp",
+        function(frame, button)
+            --print(frame:GetName(), button)
+        end
+    )
+
+    InFlightTimerFrame:SetScript(
+        "OnDragStart",
+        function(frame)
+            if IsShiftKeyDown() then
+                frame:StartMoving()
+            end
+        end
+    )
+    InFlightTimerFrame:SetScript(
+        "OnDragStop",
+        function(frame)
+            frame:StopMovingOrSizing()
+            local a, b, c, d, e = frame:GetPoint()
+            db.p, db.rp, db.x, db.y = a, c, floor(d + 0.5), floor(e + 0.5)
+            local anchorPoint, relativeTo, relativePoint, offsetX, offsetY = frame:GetPoint()
+            db.profile.barLocation.anchorPoint = anchorPoint
+            db.profile.barLocation.relativePoint = relativePoint
+            db.profile.barLocation.offsetX = offsetX
+            db.profile.barLocation.offsetY = offsetY
+        end
+    )
+    self.InFlightTimerFrame = InFlightTimerFrame
+end
+
+function statusBarModuleCore:StartTimerBar(text, duration, unknownFlightFlag) --Bar Text and Duration in seconds--
+    if not self.InFlightTimerFrame then
+        return
+    end
+    if (not text) or (not duration) then
+        return
+    end
+
+    local timerFrame = self.InFlightTimerFrame
+    timerFrame.duration = duration
+    timerFrame.timeRemaining = duration
+    timerFrame.text = text
+
+    local statusBar = _G[InFlightTimerFrame:GetName() .. "StatusBar"]
+    statusBar:SetMinMaxValues(0, duration)
+    statusBar:SetValue(duration)
+
+    if unknownFlightFlag then
+        statusBar:SetStatusBarColor(db.profile.unknownFlightColor.r, db.profile.unknownFlightColor.g, db.profile.unknownFlightColor.b, db.profile.unknownFlightColor.a)
+    else
+        statusBar:SetStatusBarColor(db.profile.barColor.r, db.profile.barColor.g, db.profile.barColor.b, db.profile.barColor.a)
+    end
+
+    local displayText = _G[timerFrame:GetName() .. "Text"]
+    displayText:SetFormattedText("%s - %s", text, disp_time(duration))
+
+    timerFrame:Show()
+end
+
+function statusBarModuleCore:StopTimerBar()
+    self.InFlightTimerFrame:Hide()
+    self.InFlightTimerFrame.timeRemaining = 0
+    self.InFlightTimerFrame.duration = 0
+    self.InFlightTimerFrame.text = ""
+    _G[self.InFlightTimerFrame:GetName() .. "Text"]:SetText("")
+end
+
+
+function statusBarModuleCore:GetOption(info)
+    if info.type == "color" then
+        local t = db.profile[info[#info]]
+        if t then
+            return t.r, t.g, t.b, t.a
+        else
+            return math.random(0, 100) / 100, math.random(0, 100) / 100, math.random(0, 100) / 100, math.random(0, 100) / 100
+        end
+    else
+        return db.profile[info[#info]]
+    end
+end
+function statusBarModuleCore:SetOption(info, ...)
+    if info.type == "color" then
+        local red, blue, green, alpha = ...
+        db.profile[info[#info]] = db.profile[info[#info]] or {}
+        db.profile[info[#info]].r = red
+        db.profile[info[#info]].b = blue
+        db.profile[info[#info]].g = green
+        db.profile[info[#info]].a = alpha
+    else
+        db.profile[info[#info]] = ...
+    end
+end
+
+addonCore.configOptionsTable.plugins = addonCore.configOptionsTable.plugins or {}
+addonCore.configOptionsTable.plugins["StatusBarModule"] = {
+    barOptions = {
+        handler = statusBarModuleCore,
+        get = "GetOption",
+        set = "SetOption",
+        name = L["Bar Options"],
+        type = "group",
+        order = 100,
+        args = {
+            countUp = {
+                hidden = true,
+                name = L["Count Upwards"],
+                type = "toggle",
+                order = 10
+            },
+            fillUp = {
+                hidden = true,
+                name = L["Fill Upwards"],
+                type = "toggle",
+                order = 20
+            },
+            showSpark = {
+                hidden = true,
+                name = L["Show Spark"],
+                type = "toggle",
+                order = 30
+            },
+            backgroundColor = {
+                hidden = true,
+                name = L["Background Color"],
+                type = "color",
+                order = 40
+            },
+            unknownFlightColor = {
+                name = L["Unknown Flight Color"],
+                type = "color",
+                order = 50
+            },
+            size = {
+                hidden = true,
+                name = L["Bar Size Settings"],
+                type = "group",
+                order = 60,
+                inline = true,
+                args = {
+                    barHeight = {
+                        name = L["Height"],
+                        type = "range",
+                        order = 10,
+                        min = 10,
+                        max = 300,
+                        step = 1
+                    },
+                    barWidth = {
+                        name = L["Width"],
+                        type = "range",
+                        order = 20,
+                        min = 10,
+                        max = 1000,
+                        step = 10
+                    }
+                }
+            },
+            barSettings = {
+                name = L["Bar Settings"],
+                type = "group",
+                order = 70,
+                inline = true,
+                width = "normal",
+                args = {
+                    barTexture = {
+                        name = L["Bar Texture"],
+                        type = "select",
+                        order = 10,
+                        dialogControl = "LSM30_Statusbar",
+                        values = AceGUIWidgetLSMlists.statusbar
+                    },
+                    barColor = {
+                        name = L["Bar Color"],
+                        type = "color",
+                        order = 20
+                    }
+                }
+            },
+            borderSettings = {
+                hidden = true,
+                name = L["Border Settings"],
+                type = "group",
+                order = 80,
+                inline = true,
+                width = "normal",
+                args = {
+                    borderTexture = {
+                        name = L["Border Texture"],
+                        type = "select",
+                        order = 10,
+                        dialogControl = "LSM30_Border",
+                        values = AceGUIWidgetLSMlists.border
+                    },
+                    borderColor = {
+                        name = L["Border Color"],
+                        type = "color",
+                        order = 20
+                    }
+                }
+            }
+        }
+    },
+    textOptions = {
+        handler = statusBarModuleCore,
+        get = "GetOption",
+        set = "SetOption",
+        name = "Text Options",
+        type = "group",
+        order = 200,
+        args = {
+            compactMode = {
+                name = L["Compact Mode"],
+                type = "toggle",
+                order = 10
+            },
+            fontOptions = {
+                name = L["Font Options"],
+                type = "group",
+                inline = true,
+                args = {
+                    fontName = {
+                        name = L["Font"],
+                        type = "select",
+                        dialogControl = "LSM30_Font",
+                        values = AceGUIWidgetLSMlists.font,
+                        order = 10
+                    },
+                    fontSize = {
+                        name = L["Size"],
+                        type = "input",
+                        order = 20
+                    },
+                    fontColor = {
+                        name = L["Color"],
+                        type = "color",
+                        order = 30
+                    }
+                }
+            }
+        }
+    }
+}
